18493115baefc6361269066718a121c0443c93aa
kid_1412 <lbs1019@qq.com>
2019-05-02 16:07:33 +0800

    all file fixed

--- src/main/java/com/lbs/aspect/LogAspect.java
+++ src/main/java/com/lbs/aspect/LogAspect.java
@@ -1,9 +1,7 @@
 package com.lbs.aspect;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -18,7 +16,7 @@ import java.util.Date;
 public class LogAspect {
     private static final Logger logger = LoggerFactory.getLogger(LogAspect.class);
 
-    @Before("execution(* com.lbs.controller.*Controller.*(..))")
+//    @Before("execution(* com.lbs.controller.*Controller.*(..))")
     public void beforeMethod(JoinPoint joinPoint) {
         StringBuilder sb = new StringBuilder();
         for (Object arg : joinPoint.getArgs()) {
@@ -29,7 +27,7 @@ public class LogAspect {
         logger.info("before method:" + sb.toString());
     }
 
-    @After("execution(* com.lbs.controller.*Controller.*(..))")
+//    @After("execution(* com.lbs.controller.*Controller.*(..))")
     public void afterMethod() {
         logger.info("after method " + new Date());
     }

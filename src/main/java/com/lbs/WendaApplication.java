b18928a3f4e14bf48d81e6b21a4f0bd7262be92b
lbs1412 <liabing1019@gmail.com>
2019-05-06 20:36:46 +0800

    update

--- src/main/java/com/lbs/WendaApplication.java
+++ src/main/java/com/lbs/WendaApplication.java
@@ -5,21 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class WendaApplication extends SpringBootServletInitializer {
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(WendaApplication.class);
-	}
-	public static void main(String[] args) {
-		SpringApplication.run(WendaApplication.class, args);
-	}
-}
-
-
 //@SpringBootApplication
-//public class WendaApplication {
+//public class WendaApplication extends SpringBootServletInitializer {
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		return builder.sources(WendaApplication.class);
+//	}
 //	public static void main(String[] args) {
 //		SpringApplication.run(WendaApplication.class, args);
 //	}
 //}
+
+
+@SpringBootApplication
+public class WendaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WendaApplication.class, args);
+	}
+}
